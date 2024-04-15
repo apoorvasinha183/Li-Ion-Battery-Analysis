d9f3cd0eb9c52ac8f61a831507fdf835c32766a3
asinha377 <asinha377@gatech.edu>
2024-04-15 11:21:51 -0400

    train_mlp.py works

--- TF/train_mlp.py
+++ TF/train_mlp.py
@@ -11,7 +11,15 @@ from battery_data import getDischargeMultipleBatteries
 
 DTYPE = 'float64'
 tf.keras.backend.set_floatx(DTYPE)
+#See if this works for initialization
+class resetStateCallback(tf.keras.callbacks.Callback):
+    def __init__(self, initial_state=None):
+        self.initial_state=initial_state
 
+    def on_epoch_begin(self, epoch, logs=None):
+        # if self.initial_state is None:
+        self.initial_state = self.model.layers[0].cell.get_initial_state(batch_size=self.model.input.shape[0])
+        self.model.layers[0].reset_states(self.initial_state)
 # load battery data
 data_RW = getDischargeMultipleBatteries()
 max_idx_to_use = 3
@@ -55,7 +63,7 @@ for row in np.argwhere((target<EOD) | (np.isnan(target))):
         target_shiffed[row[0]] = np.ones(time_window_size) * target[row[0]][0]
         target_shiffed[row[0]][time_window_size-row[1]:] = target[row[0]][:row[1]]
 
-
+#TRAIN-TEST SPLIT
 val_idx = np.linspace(0,35,6,dtype=int)
 train_idx = [i for i in np.arange(0,36) if i not in val_idx]
 print(train_idx)
@@ -65,7 +73,8 @@ time_window_size = inputs.shape[1]  # 310
 print("this is ",time_window_size)
 model = get_model(batch_input_shape=(inputs[train_idx,:,:].shape[0],time_window_size,inputs.shape[2]), dt=dt, mlp=True, share_q_r=False, stateful=True)
 #model.compile(optimizer=tf.keras.optimizers.Adam(lr=1e-3), loss="mse", metrics=["mae"], sample_weight_mode="temporal")
-model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=3e-7), loss="mse", metrics=["mae"])
+#model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=1e-3), loss="mse", metrics=["mae"])
+model.compile(optimizer=tf.keras.optimizers.Adam(lr=2e-2), loss="mse", metrics=["mae"])
 model.summary()
 #huzzah
 # samples_intervals           = [100, 100, 150, 100, 200,  50,  40, max_size-740]
@@ -126,18 +135,18 @@ scheduler_cb = tf.keras.callbacks.LearningRateScheduler(scheduler)
 EPOCHS = 1000
 
 # callbacks = [model_checkpoint_callback,scheduler_cb, reduce_lr_on_plateau]
-callbacks = [model_checkpoint_callback,reduce_lr_on_plateau]
-# callbacks = []
+callbacks = [model_checkpoint_callback,reduce_lr_on_plateau,resetStateCallback()]
+#callbacks = []
 
 #load pre-trained weights
 # model.set_weights(np.load('./training/mlp_all_avg_weights.npy',allow_pickle=True))
-print(np.shape(inputs_shiffed))
-print(np.shape(target_shiffed))
+#print(np.shape(inputs_shiffed))
+#print(np.shape(target_shiffed))
 BLOCK = False
 if not BLOCK:
     start = time()
-    history = model.fit(inputs_shiffed, target_shiffed[:,:,np.newaxis], epochs=EPOCHS, callbacks=callbacks, shuffle=False, sample_weight=sample_weight)
-    #history = model.fit(inputs_shiffed[train_idx,:,:], target_shiffed[train_idx,:,np.newaxis], epochs=EPOCHS, callbacks=callbacks, shuffle=False)
+    #history = model.fit(inputs_shiffed, target_shiffed[:,:,np.newaxis], epochs=EPOCHS, callbacks=callbacks, shuffle=False, sample_weight=sample_weight)
+    history = model.fit(inputs_shiffed[train_idx,:,:], target_shiffed[train_idx,:,np.newaxis], epochs=EPOCHS, callbacks=callbacks, shuffle=False)
     duration = time() - start
     print("Train time: {:.2f} s - {:.3f} s/epoch ".format(duration, duration/EPOCHS))
 
