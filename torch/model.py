5e04a206700e0ebb5e87f5daeaaa73ad54ef5350
asinha377 <asinha377@gatech.edu>
2024-04-19 08:21:25 -0400

    Power and ageing data generation

--- torch/model.py
+++ torch/model.py
@@ -3,28 +3,26 @@ from time import time
 import matplotlib
 import matplotlib.pyplot as plt
 import tensorflow as tf
-
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
 DTYPE = 'float64'
 
 #from BatteryRNNCell import BatteryRNNCell
-from BatteryRNNCell_mlp import BatteryRNNCell 
+from BatteryRNNCell_mlp import BatteryRNNCell,BatteryRNN
 
 def get_model(batch_input_shape=None, return_sequences=True, stateful=False, dtype=DTYPE, dt=1.0, mlp=True, mlp_trainable=True, share_q_r=True, q_max_base=None, R_0_base=None, D_trainable=False):
-    model = tf.keras.Sequential()
-    model.add(tf.keras.layers.InputLayer(batch_input_shape=batch_input_shape))
-    if mlp:
-        batch_size = batch_input_shape[0]
-        if share_q_r:
-            batch_size = 1
-        model.add(tf.keras.layers.RNN(BatteryRNNCellMLP(dtype=dtype, dt=dt, mlp_trainable=mlp_trainable, batch_size=batch_size, q_max_base=q_max_base, R_0_base=R_0_base, D_trainable=D_trainable), return_sequences=return_sequences, stateful=stateful,dtype=dtype))
-    else:
-        model.add(tf.keras.layers.RNN(BatteryRNNCell(dtype=dtype, dt=dt), return_sequences=return_sequences, stateful=stateful,dtype=dtype))
-
+    #Only mlp ,pure physics not needed
+    batch_size = batch_input_shape[0]
+    if share_q_r:
+        batch_size = 1
+    model = BatteryRNN(mlp_trainable=mlp_trainable,dt=dt,q_max_base=q_max_base,R_0_base=R_0_base,)
     return model
 
 if __name__ == "__main__":
     # test keral model pred
-
+    #TODO: Replicate this in pytorch
     inputs = np.ones((700,1000), dtype=DTYPE) * np.linspace(1.0,2.0,1000)  # constant load
     inputs = inputs.T[:,:,np.newaxis]
 
